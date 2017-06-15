cbfbeefab166ccf66136864ae5898eb5349a3437
pipe <yys320carter@163.com>
2017-06-15 15:35:29 +0800

    添加设置 router-view key，计算key值，实现原来 watch router对象来处理 不同页面同组件 路由不刷新问题
    新增eslintrc 规则，max-len 小于 120

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // allow max line length 120
     'max-len': ["error", 120]
   }
 }
