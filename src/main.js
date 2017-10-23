38cebcb9b950280f9e8762e16417e0d38e9cc6cb
pipe <yys320carter@163.com>
2017-10-23 16:32:54 +0800

    api fetch

--- src/main.js
+++ src/main.js
@@ -1,30 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import store from './store';
-import './utils/progress';
-import './utils/validate';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import './utils/progress'
+import './utils/validate'
 import './utils/functions'
 import './utils/components'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requireAuth)) {
     if (!store.state.auth) {
       next({
         path: '/login',
-        query: { redirect: to.fullPath },
-      });
+        query: {redirect: to.fullPath},
+      })
     } else {
-      next();
+      next()
     }
   } else {
-    next();
+    next()
   }
-});
+})
 
 /* eslint-disable no-new */
 new Vue({
@@ -32,5 +32,5 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App },
-});
+  components: {App},
+})
